faf237418a8444d5ee6b457075c6f601e55798c7
LittleEngineerTT <wesleyg97322@gmail.com>
2023-10-20 19:56:14 +0200

    try to upgrade response rate

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ from my_telegram import *
 
 
 def main():
-    new_telegram_run()
-
+    while True:
+        new_telegram_run()
 
 if __name__ == '__main__':
-    #signal.signal(signal.SIGTERM, sys.exit())
     main()
