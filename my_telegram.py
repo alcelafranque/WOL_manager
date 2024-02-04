6b919cf83d14cde1e860986a19a9f73d01087742
LittleEngineerTT <wesleyg97322@gmail.com>
2024-02-04 14:11:46 +0100

    feat: wip status

--- my_telegram.py
+++ my_telegram.py
@@ -99,10 +99,8 @@ def delete_mac_from_name(name, filename):
 async def status(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     last_message = update.message.text
     if re.match(r"/status\s{1,}[^\s]+\s{0,}", last_message):
-        await update.message.reply_text("OUI")
         device_name = last_message.split(" ")[-1] if last_message.split(" ")[-1] else last_message.split(" ")[-2]
         mac, interface = get_data_from_name(device_name, name_to_mac_file)
-        await update.message.reply_text("MAC:" + str(mac))
         started = status_checker(mac, 0, config)
         if not started:
             text = "This ressource is down"
@@ -162,7 +160,6 @@ async def select_device(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
             text = "Starting"
             await update.message.reply_text(text)
         elif context.user_data.get("action") == "status":
-            await update.message.reply_text("MAC:" + str(mac))
             started = status_checker(mac, 0, config)
             if not started:
                 text = "This ressource is down"
