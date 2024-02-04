692b40ad6cc713e65c7f685851757cf2f462a379
LittleEngineerTT <wesleyg97322@gmail.com>
2024-02-04 11:55:43 +0100

    fix: router ip address
    fix: hardcoded values

--- wake_me_up.py
+++ wake_me_up.py
@@ -1,9 +1,13 @@
 import paramiko
 
 
-def wake_me_up(mac, ssh_filename, mdp, interface):
+def wake_me_up(mac, config, interface):
     ssh = paramiko.SSHClient()
+    ssh_filename = config["path_to_private_key"]
+    mdp = config["ssh_password"]
+    router_ip = config["router_ip"]
+    router_hostname = config["router_hostname"]
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect("10.240.128.254", username='wol', password=mdp, key_filename=ssh_filename)
+    ssh.connect(router_ip, username=router_hostname, password=mdp, key_filename=ssh_filename)
     stdin, stdout, stderr = ssh.exec_command("sudo /usr/sbin/etherwake -i " + interface + " " + mac)
     ssh.close()
