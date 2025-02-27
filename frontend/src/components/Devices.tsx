173c74928887e3e09b90481297f5f8169b7c3d68
wesleyg97322 <wesley@lac-coloc.fr>
2025-02-27 14:59:56 +0100

    update(frontend)(devices): display 3 names per line

--- frontend/src/components/Devices.tsx
+++ frontend/src/components/Devices.tsx
@@ -1,17 +1,26 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {send_request} from "../utils/requests";
 import config from "../config.d/config.yaml";
+import Grid from '@mui/material/Grid2';
 
+import {Device} from '../utils/types';
 
 interface DevicesProps {
 
 }
 
 export const Devices: React.FC<DevicesProps> = () => {
+    const [devices, setDevices] = useState<Array<Device>>([]);
 
     const get_devices = async () => {
         const response = await send_request(config.backend_url, "devices", {});
-        console.log(await response.json());
+        const json_response = await response.json();
+        console.log(json_response);
+
+        if ("devices" in json_response)
+        {
+            setDevices(json_response["devices"]);
+        }
     }
 
     useEffect(() => {
@@ -20,7 +29,16 @@ export const Devices: React.FC<DevicesProps> = () => {
     }, [])
 
     return (
-        <>
-        </>
+        <Grid padding={"10px"} container rowSpacing={1} sx={{
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    alignContent: 'center'
+                }}>
+            {devices.map((device, index) => (
+                <Grid size={4} key={index}>
+                    {device.hostname}
+                </Grid>
+            ))}
+        </Grid>
     )
 }
