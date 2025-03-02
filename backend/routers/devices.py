fd5c3aadbac6c13b4d87ff2da4a5ba8c89217460
wesleyg97322 <wesley@lac-coloc.fr>
2025-03-02 20:43:52 +0100

    feat(backend)(devicees): status route

--- backend/routers/devices.py
+++ backend/routers/devices.py
@@ -2,6 +2,11 @@ from libs.devices import get_devices, status_checker
 from core.config import get_config
 
 from fastapi import APIRouter
+from pydantic import BaseModel
+
+
+class Mac(BaseModel):
+    mac: str
 
 devices = APIRouter(
     prefix="",
@@ -15,3 +20,9 @@ config = get_config()
 def retrieve_devices():
     devices = get_devices()
     return {"devices": devices}
+
+@devices.post("/status")
+def get_status(mac: Mac):
+    print(mac)
+    status = status_checker(mac.mac, config)
+    return {"status": status}
