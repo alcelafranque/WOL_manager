ac432672cd764793caebf11f20ac015d01b93dae
wesleyg97322 <wesley@lac-coloc.fr>
2025-03-04 18:58:14 +0100

    update(frontend): add component that update dynamically input

--- frontend/src/utils/Devices/DeviceRegister.tsx
+++ frontend/src/utils/Devices/DeviceRegister.tsx
@@ -1,14 +1,48 @@
 import {Device} from "../types";
-import React from "react";
+import React, {useState, ChangeEvent} from "react";
 import {TextField} from "@mui/material";
 import Grid from '@mui/material/Grid2';
+
+
+interface FieldEntryProps {
+    title: string;
+    inputData: string;
+    setInputData: React.Dispatch<React.SetStateAction<string>>;
+}
+
+
+const FieldEntry: React.FC<FieldEntryProps> = ({inputData, setInputData, title}) => {
+    const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInputData(event.target.value);
+    };
+
+    return (
+        <>
+            <Grid size={4} sx={{
+                display: 'flex',
+                alignContent: 'center',
+                alignItems: 'center',
+                justifyContent: 'center'
+            }}>
+                {title}:
+            </Grid>
+            <Grid size={8} >
+                <TextField value={inputData} onChange={handleInput}/>
+            </Grid>
+        </>
+    )
+}
+
+
 
 interface DeviceRegisterProps {
     setDevices: React.Dispatch<React.SetStateAction<Array<Device>>>;
 }
 
 export const DeviceRegister: React.FC<DeviceRegisterProps> = ({setDevices}) => {
-    const fields = ["Hostname", "Mac", "interface"]
+  const [hostname, setHostname] = useState<string>("");
+  const [mac, setMac] = useState<string>("");
+  const [deviceInterface, setDeviceInterface] = useState<string>("");
 
     return (
         <div style={{
@@ -19,23 +53,9 @@ export const DeviceRegister: React.FC<DeviceRegisterProps> = ({setDevices}) => {
                 border: '1px solid',
                 padding: '10px'
             }}>
-                {fields.map(field => (
-                    <>
-                        <Grid size={4} sx={{
-                            display: 'flex',
-                            alignContent: 'center',
-                            alignItems: 'center',
-                            justifyContent: 'center'
-                        }}>
-                            {field}:
-                        </Grid>
-                        <Grid size={8}>
-                            <TextField sx={{
-                            }}/>
-                        </Grid>
-                    </>
-                ))}
-
+                <FieldEntry inputData={hostname} setInputData={setHostname} title={"Hostname"}/>
+                <FieldEntry inputData={mac} setInputData={setMac} title={"Mac"}/>
+                <FieldEntry inputData={deviceInterface} setInputData={setDeviceInterface} title={"Interface"}/>
             </Grid>
         </div>
     );
